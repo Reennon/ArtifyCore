937f8ee1e0d58348e04cfdefb239f348ce674bc2
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2020-11-28 02:34:19 +0200

    roman's branch added old files

---
+++ RuntimeCore/ModuleHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RuntimeCore
+{
+    internal static partial class ModuleHandler
+    {
+        public static readonly Dictionary<Type, ILinkerBaseFields> Modules = new()
+        {
+            {typeof(Dispatcher), Dispatcher.GetInstance()},
+            {typeof(InputHandler), new InputHandler()}
+        };
+    }
+    
+    internal static partial class IOHandler<T>
+        where T : ILinkerBaseFields
+    {
+        //private static readonly ILinkerBaseFields LinkedTo = typeof(T).;
+
+        internal static void TInputInvoke( String command) 
+        {
+            (ModuleHandler
+                .Modules[typeof(T)] is T ? (T) ModuleHandler
+                .Modules[typeof(T)] : default)?.InputInvoker(command);
+            
+        }   
+    
+    
+        internal static String TIOutputInvoke(T type, String command) 
+        {
+            return (ModuleHandler
+                .Modules[typeof(T)] is T ? (T) ModuleHandler
+                .Modules[typeof(T)] : default)?.OutputInvoker(command);
+            
+        }  
+        
+    
+    }
+}
