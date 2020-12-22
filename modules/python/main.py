bc43196f870a9a3f02ef1168c6aca0e29d47f334
wwwwwww <mik373737373737@gmail.com>
2020-12-22 19:02:21 +0200

    refactoring python script

--- modules/python/main.py
+++ modules/python/main.py
@@ -20,35 +20,44 @@ import cv2
 import json
 import sys
 
-#Your errors
-Error = 0
-try:
 
-    #Input data(we parse it)
- app_json = json.loads(sys.argv[1])
- 
-    #Number of image
- numberOfImage = app_json['numberOfImages']
- arr = []
-    #Add your filePATH to array
- for i in range(numberOfImage):
-    arr.append(app_json['Image'][i])
-
-    # Your image
- img = cv2.imread(arr[0])
+def my_algorithm(arr):
+    # Your image arr[1],arr[2],....
+    img = cv2.imread(arr[1])
 
     # algorithms
- img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+    img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
 
     # Store the resultant image
- status = cv2.imwrite(arr[0], img)
+    status = cv2.imwrite(arr[1], img)
+
+    # Return your result, which will return in json
+    return status
+    
+
+try:
+    #Your errors
+    Error = 0
+
+    #Input data(we parse it)
+    app_json = json.loads(sys.argv[1])
+ 
+    #Number of image
+    numberOfImage = app_json['numberOfImages']
+    arr = []
+
+    #Add your filePATH to array
+    for i in range(numberOfImage):
+        arr.append(app_json['Image'][i])
+
+    status = my_algorithm(arr)
  
 except:
     Error = 1
 finally:
     try:
         dict={}
-        #Hear write your result(MUST BE "EEError" not "Error")
+        #Hear write your result
         dict['EEError'] = Error
         dict['Result'] = status
         
